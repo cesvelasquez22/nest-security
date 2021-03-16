a8ac8336264b79949678f3626460353cffce95b5
Cesar Velásquez Cordova <ces.velasquez@hotmail.com>
2021-03-16 09:19:10 -0600

    Extending guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/constants';
 import { AuthService } from './auth/services/auth/auth.service';
 import { JwtAuthGuard } from './core/jwt-auth.guard';
 import { LocalAuthGuard } from './core/local-auth.guard';
@@ -9,17 +10,18 @@ export class AppController {
   constructor(private readonly appService: AppService, private auhtService: AuthService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @UseGuards(LocalAuthGuard)
+  @Public()
   @Post('auth/login')
   async login(@Request() req) {
     return this.auhtService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/core/jwt-auth.guard';
 import { UsersModule } from 'src/users/users.module';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
@@ -16,7 +18,15 @@ import { AuthService } from './services/auth/auth.service';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
